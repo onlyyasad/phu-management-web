b929cbc0ad74389f62b5f13f75e55a53f6fe633f
hafij <hafij.alasad23@gmail.com>
2026-01-23 02:02:56 +0600

    feat: refresh token and logout after refresh implemented

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,7 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
+import { logout, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -14,8 +15,32 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+
+    if (data) {
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(setUser({ user, token: data.data.accessToken }));
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
--- src/utils/verifyToken.ts
+++ src/utils/verifyToken.ts
@@ -2,6 +2,5 @@ import { jwtDecode } from "jwt-decode";
 
 export const verifyToken = (token: string) => {
   const decoded = jwtDecode(token);
-  console.log({decoded});
   return decoded;
 };
